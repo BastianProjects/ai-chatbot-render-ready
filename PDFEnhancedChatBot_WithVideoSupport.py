a2bebe6c12499bf592b3108925b6b9ac6c5b5843
Bastian Jose <bastianjose.k@gmail.com>
2025-05-14 23:20:38 +0300

    change

--- PDFEnhancedChatBot_WithVideoSupport.py
+++ PDFEnhancedChatBot_WithVideoSupport.py
@@ -14,15 +14,15 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
-options = Options()
-options.binary_location = "/usr/bin/google-chrome"
-options.add_argument("--headless")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
+chrome_options = Options()
+chrome_options.binary_location = "/usr/bin/google-chrome"
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
 
 service = Service("/usr/local/bin/chromedriver")
 
-driver = webdriver.Chrome(service=service, options=options)
+driver = webdriver.Chrome(service=service, options=chrome_options)
 driver.get("https://example.com")
 print(driver.title)
 driver.quit()

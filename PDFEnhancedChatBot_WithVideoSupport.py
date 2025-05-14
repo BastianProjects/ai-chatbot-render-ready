960f1f4bfc8f9a9e3dd76f9b8ec7165e1da7f69d
Bastian Jose <bastianjose.k@gmail.com>
2025-05-14 23:40:04 +0300

    change

--- PDFEnhancedChatBot_WithVideoSupport.py
+++ PDFEnhancedChatBot_WithVideoSupport.py
@@ -11,20 +11,9 @@ from langchain.vectorstores import Chroma
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema import Document
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 
-chrome_options = Options()
-chrome_options.binary_location = "/usr/bin/google-chrome"
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
-
-service = Service("/usr/local/bin/chromedriver")
-
-driver = webdriver.Chrome(service=service, options=chrome_options)
-driver.get("https://example.com")
-print(driver.title)
+driver = webdriver.Chrome()
+driver.get("https://www.google.com/")
 driver.quit()
 
 # -- Settings --
